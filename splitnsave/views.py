4d8922a34d5050fa51bcf917abed63ac2eef98c3
devarsh13 <devarshsheth13@gmail.com>
2016-04-03 16:40:46 +0530

    js

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -5,7 +5,7 @@ from models import *
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
-
+@csrf_exempt
 def signup_check(request):
 	Email=request.POST.get('Email','')
 	
