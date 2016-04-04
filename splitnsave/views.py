f98ba441563329d7d4b0d8ee02f1e370ef408803
devarsh13 <devarshsheth13@gmail.com>
2016-04-05 01:40:46 +0530

    Fixed csrf issue

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from models import *
 from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
