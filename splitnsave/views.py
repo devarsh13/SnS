3619a3230bc8ae90cd6573285157a5f7cefe72a7
devarsh13 <devarshsheth13@gmail.com>
2016-04-05 14:23:48 +0530

    Fixed csrf issue

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -8,7 +8,7 @@ import ast
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
-@csrf_exempt
+
 def signup_check(request):
 	Email=request.POST.get('Email','')
 	
@@ -25,7 +25,7 @@ def signup_check(request):
 		msg['status']='-1'
 
 	return JsonResponse(msg)
-@csrf_exempt
+
 def signup(request):
 	input1=request.body.decode('utf-8')
 	input1=json.loads(input1)
@@ -41,7 +41,7 @@ def signup(request):
 		profession=input1['Profession_Name'],
 		image_url=input1['Image_Link'])
 	
-@csrf_exempt
+
 def editprofile(request):
 	userid=request.POST.get('input','')
 	userid=userid['User_id']
@@ -49,7 +49,7 @@ def editprofile(request):
 	d={'user_id':u.user_id,'first_name':u.first_name,'last_name':u.last_name,'email':u.email,'password':u.password,'verified':u.verified,'contact_number':u.contact_number,'city':u.city.city_name,'institute':u.institute.institute_name,'birthday':u.birthday,'profession':u.profession.profession_name,'gender':u.gender,'status':u.status}
 	return JsonResponse(d)
 
-@csrf_exempt
+
 def edit_profile_change(request):
 	data_json=request.POST.get('input','')
 	u=users.objects.get(user_id=data_json['User_id'])
@@ -64,33 +64,33 @@ def edit_profile_change(request):
 	u.profession=input1['Profession_Name'],
 	u.image_url=input1['Image_Link']
 
-@csrf_exempt
+
 def userprofile(request):
 	userid='0'
 	u=users.objects.get(user_id=userid)
 	d={'user_id':u.user_id,'first_name':u.first_name,'last_name':u.last_name,'email':u.email,'password':u.password,'verified':u.verified,'contact_number':u.contact_number,'city':u.city.city_name,'institute':u.institute.institute_name,'birthday':u.birthday,'profession':u.profession.profession_name,'gender':u.gender,'status':u.status}
 	return JsonResponse(d)
 
-@csrf_exempt
+
 def settings(request):
 	userid='0'
 	u=users.objects.get(user_id=userid)
 	d={'old_password':u.password}
 	return JsonResponse(d)
 
-@csrf_exempt
+
 def change_password(request):
 	userid=request.POST.get('userid','')
 	new_password=request.POST.get('new_password','')
 	u=user.objects.get(user_id=userid)
 	u.password=new_password
 
-@csrf_exempt
+
 def dashboard(request):
 	#Coded in /SEN
 	pass
 
-@csrf_exempt	
+	
 def products(request):
 	#Coded in /SEN
 	pass
