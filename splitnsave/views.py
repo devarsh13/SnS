d8e0c0522dd2fca7b51e56ab5ca08601684fa090
devarsh13 <devarshsheth13@gmail.com>
2016-04-06 23:55:09 +0530

    Frontend Integration

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -49,27 +49,33 @@ def signup(request):
 	return JsonResponse({status:'0'})
 @csrf_exempt
 def editprofile(request):
-	userid=request.POST.get('input','')
-	userid=userid['User_id']
-	u=users.objects.get(user_id=userid)
+	input1=json.loads(request.body)
+	Email=input1['Email']
+	u=users.objects.get(email=Email)
 	d={'user_id':u.user_id,'first_name':u.first_name,'last_name':u.last_name,'email':u.email,'password':u.password,'verified':u.verified,'contact_number':u.contact_number,'city':u.city.city_name,'institute':u.institute.institute_name,'birthday':u.birthday,'profession':u.profession.profession_name,'gender':u.gender,'status':u.status}
 	return JsonResponse(d)
 
 @csrf_exempt
 def edit_profile_change(request):
-	data_json=request.POST.get('input','')
-	u=users.objects.get(user_id=data_json['User_id'])
-	u.first_name=input1['First_Name'],
-	u.last_name=input1['Last_Name'],
-	u.contact_number=input1['ContactNumber'],
-	u.password=input1['Password'],
-	u.city_name=input1['CityName'],
-	u.birthday=input1['Birthdate'],
-	u.gender=input1['Gender'],
-	u.institute=input1['Institute_Name'],
-	u.profession=input1['Profession_Name'],
+	input1=json.loads(request.body)
+	Email=input1['Email']
+	institute=institute_list.objects.get(institute_name=input1['Institute_Name'])
+	profession=profession_list.objects.get(profession_name=input1['Profession_Name'])
+	city=city_list.objects.get(city_name=input1['CityName'])
+	birthdate=strptime(input1['Birthdate'],'%d/%m/%Y')
+	u=users.objects.get(email=Email)
+	u.first_name=input1['First_Name']
+	u.last_name=input1['Last_Name']
+	u.contact_number=input1['ContactNumber']
+	u.password=input1['Password']
+	u.city_name=city
+	u.birthday=str(birthdate.tm_year)+'-'+str(birthdate.tm_mon)+'-'+str(birthdate.tm_mday)
+	u.gender=input1['Gender']
+	u.institute=institute
+	u.profession=profession
 	u.image_url=input1['Image_Link']
 
+	return JsonResponse({status:'0'})
 @csrf_exempt
 def userprofile(request):
 	userid='0'
