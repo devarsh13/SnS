8e0d82ac2d10b7fcd014ccc5439922bad3bbecff
devarsh13 <devarshsheth13@gmail.com>
2016-04-11 01:04:46 +0530

    Added Delete Functionality

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -207,7 +207,7 @@ def update_my_posts(request):
 	ui=user_interested.objects.get(user_id=u,product_id=p)
 	ui.status=Status
 	ui.save()
-	if(Status==2):
+	if(Status=='2'):
 		ui.delete()
 	return JsonResponse({'status':0})
 @csrf_exempt
