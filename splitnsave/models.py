49ebfde55686fa13d8cfc18b61d34a90d07b9dc9
devarsh13 <devarshsheth13@gmail.com>
2016-04-10 23:17:41 +0530

    update

--- splitnsave/models.py
+++ splitnsave/models.py
@@ -105,7 +105,7 @@ class products(models.Model):
 	image_url=models.URLField(default=0)
 	price=models.IntegerField(default=0,null=False)
 	confirm_date=models.DateField(null=True,default=None)
-	post_date=models.DateField(default=None)
+	post_date=models.DateField(default=None,null=True)
 	location=models.CharField(max_length=100,default=None,null=True)
 	def __str__(self):
 		return str(self.product_id)
