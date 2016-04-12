7aef47fb860bf9d0101782ff53d5c9c8544ccbec
devarsh13 <devarshsheth13@gmail.com>
2016-04-12 19:43:24 +0530

    updated

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -750,7 +750,7 @@ def send_request(request):
 	Product_Id=input1['Product_Id']
 	p=products.objects.get(product_id=Product_Id)
 	u=users.objects.get(email=Email)
-	user_interested.objects.create(product_id=p,user_id=u,tatus=1)
+	user_interested.objects.create(product_id=p,user_id=u,status=1)
 
 
 	return JsonResponse({'status':0})
