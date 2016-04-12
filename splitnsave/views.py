1dc5dba58482159b5707a2df88f8c86b6ecacd2b
devarsh13 <devarshsheth13@gmail.com>
2016-04-13 00:38:31 +0530

    updated

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -234,7 +234,7 @@ def update_my_posts(request):
 	ui=user_interested.objects.get(user_id=u,product_id=p)
 	ui.status=Status
 	ui.save()
-	if(ui.status=='2' or ui.status==2):
+	if(ui.status=='1' or ui.status==1):
 		ui.delete()
 	return JsonResponse({'status':0})
 
