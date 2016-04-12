6598cea6a4dda9b088f983c4cef353d41a6273f8
devarsh13 <devarshsheth13@gmail.com>
2016-04-12 16:35:43 +0530

    Updated

--- splitnsave/models.py
+++ splitnsave/models.py
@@ -263,6 +263,6 @@ class user_interested(models.Model):
 	status=models.IntegerField(default=0)
 
 class user_to_user(models.Model):
-	user1=models.ForeignKey(users,related_name='user1',default=None)
-	user2=models.ForeignKey(users,related_name='user2',default=None)
+	user1=models.ForeignKey(users,default=None,related_name='user1')
+	user2=models.ForeignKey(users,default=None,related_name='user2')
 	status=models.IntegerField(default=0)
