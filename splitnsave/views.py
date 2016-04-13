29d73c3fad5b3e4254daf13d9e31847544d8c693
devarsh13 <devarshsheth13@gmail.com>
2016-04-14 02:11:54 +0530

    updated

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -499,7 +499,7 @@ def edit_data(request):
 	Category_Id=input1['Category_Id']
 	u=users.objects.get(email=Email)
 	Product_Id=input1['Product_Id']
-	p=products.objects.get(product_id=Product_Id)
+	i=products.objects.get(product_id=Product_Id)
 	if(Category_Id=='1'):
 		category='Apartments'
 	elif(Category_Id=='2'):
