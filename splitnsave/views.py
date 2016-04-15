ff7885223bee6281e9cd241e3149dfb1810273b6
devarsh13 <devarshsheth13@gmail.com>
2016-04-15 10:08:21 +0530

    updated

--- splitnsave/views.py
+++ splitnsave/views.py
@@ -294,7 +294,7 @@ def confirm_post(request):
 	input1=json.loads(request.body)
 	Product_Id=input1['Product_Id']
 	p=products.objects.get(product_id=Product_Id)
-	p.confirm_date(datetime.now())
+	p.confirm_date=datetime.now()
 	i=p
 	u=user_interested.objects.filter(product_id=p,status=2)
 	p.status=3
