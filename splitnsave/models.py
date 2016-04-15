2762fdaeafc2383dddead81dd0176019749789e6
devarsh13 <devarshsheth13@gmail.com>
2016-04-15 11:07:44 +0530

    updated

--- splitnsave/models.py
+++ splitnsave/models.py
@@ -279,8 +279,3 @@ class chat_history(models.Model):
 	message=models.CharField(max_length=100000,default=None)
 	timestamp=models.DateTimeField(default=datetime.datetime.now())
 
-class notifications(models.Model):
-	poster=models.ForeignKey(users,default=None,related_name='n_poster')
-	seeker=models.ForeignKey(users,default=None,related_name='n_seeker')
-	product_id=models.ForeignKey(products,default=None)
-	status=models.IntegerField(default=0)
